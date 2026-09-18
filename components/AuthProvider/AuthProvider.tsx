f6805a10c0695e5e773c9fa8ce55ff5022481b51
MaksymRC1 <maksymotroshko@ukr.net>
2026-09-18 09:26:27 +0300

    Fix auth routes layout: add use client and router.refresh on mount, remove from AuthProvider

--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -10,11 +10,6 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
 
   useEffect(() => {
-    // Calling router.refresh on mount to ensure server components get the latest cookies
-    router.refresh();
-  }, [router]);
-
-  useEffect(() => {
     const initAuth = async () => {
       try {
         const response = await checkSession();
